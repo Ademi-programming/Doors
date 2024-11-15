546bd7c0c379a9f55edb8eb656e8ab09163a75df
Ademi-programming <umiir987@gmail.com>
2024-11-15 18:35:12 +0600

    Initial commit

---
+++ app/src/main/java/com/example/doors/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.doors;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView enter;
+    TextView welcome;
+    TextView register;
+    EditText email;
+    EditText password;
+    Button signIn;
+    TextView tapHere;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        enter = findViewById(R.id.enter_);
+        welcome = findViewById(R.id.tv_welcome);
+        register = findViewById(R.id.tv_register);
+        email = findViewById(R.id.et_email);
+        password = findViewById(R.id.enter_ps);
+        signIn = findViewById(R.id.btn_signIn);
+        tapHere = findViewById(R.id.ps_fg);
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+                if(email.getText().length() > 0 && password.getText().length() > 0){
+                    signIn.setBackgroundResource(R.drawable.bg_orange);
+                } else {
+                    signIn.setBackgroundResource(R.drawable.bg_grey);
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+        email.addTextChangedListener(textWatcher);
+        password.addTextChangedListener(textWatcher);
+
+        signIn.setOnClickListener(v -> {
+            String username = email.getText().toString();
+            String tvpassword = password.getText().toString();
+            if("adema".equals(username) && "nyamovna".equals(tvpassword)){
+                Toast.makeText(this, "Вы успешно зарегистрировались", Toast.LENGTH_SHORT).show();
+                welcome.setText("Добро пожаловать, " + username + "!");
+
+                welcome.setVisibility(View.VISIBLE);
+                register.setVisibility(View.GONE);
+                tapHere.setVisibility(View.GONE);
+                enter.setVisibility(View.GONE);
+                email.setVisibility(View.GONE);
+                password.setVisibility(View.GONE);
+                signIn.setVisibility(View.GONE);
+            } else {
+                Toast.makeText(this, "Неправильно ввели пароль или логин", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
